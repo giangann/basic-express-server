d88e89ccc867a8f9eb5ba4e36fa4bea2ec1ac7a5
giangann <70595717+giangann@users.noreply.github.com>
2024-07-27 04:09:30 +0700

    update route

--- dist/index.js
+++ dist/index.js
@@ -4,9 +4,30 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const port = 5000;
 const app = (0, express_1.default)();
+const route = express_1.default.Router();
+app.use((0, cors_1.default)({
+    // origin: ["http://localhost:3000", "http://localhost:8081"],
+    origin: "*",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+}));
 // default route
-app.use("/", (req, res) => { res.json("Hello, basic express server"); });
+// app.use("/", (req, res) => {
+//   res.json("Hello, basic express server");
+// });
+route.get("/me", (req, res) => {
+    const mockData = {
+        id: 1,
+        name: "JohnDue",
+    };
+    res.send(mockData);
+});
+// Use the route
+app.use("/", route);
 // start
-app.listen(port, () => { console.log('Express app running on port', port); });
+app.listen(port, () => {
+    console.log("Express app running on port", port);
+});
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,35 @@
 import Express from "express";
+import cors from "cors";
 
-const port = 5000
-const app = Express()
+const port = 5000;
+const app = Express();
+const route = Express.Router();
+app.use(
+  cors({
+    // origin: ["http://localhost:3000", "http://localhost:8081"],
+    origin: "*",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 // default route
-app.use("/",(req, res)=>{res.json("Hello, basic express server")})
+// app.use("/", (req, res) => {
+//   res.json("Hello, basic express server");
+// });
+
+route.get("/me", (req, res) => {
+  const mockData = {
+    id: 1,
+    name: "JohnDue",
+  };
+  res.send(mockData);
+});
+
+// Use the route
+app.use("/", route);
 
 // start
-app.listen(port,()=>{console.log('Express app running on port',port)})
-
+app.listen(port, () => {
+  console.log("Express app running on port", port);
+});
