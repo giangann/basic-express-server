9bdb3585518bc9c38bb2627e6582e889dfcce537
giangann <70595717+giangann@users.noreply.github.com>
2024-08-26 12:24:13 +0700

    build and rm redudant

--- dist/index.js
+++ dist/index.js
@@ -5,7 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
-const port = 5000;
+const port = 5173;
 const app = (0, express_1.default)();
 const route = express_1.default.Router();
 app.use((0, cors_1.default)({
@@ -15,15 +15,23 @@ app.use((0, cors_1.default)({
     allowedHeaders: ["Content-Type", "Authorization"],
 }));
 // default route
-// app.use("/", (req, res) => {
-//   res.json("Hello, basic express server");
-// });
+route.get("/", (req, res) => {
+    res.json("Hello, basic express server");
+});
 route.get("/me", (req, res) => {
     const mockData = {
         id: 1,
         name: "JohnDue",
+        auth: "1-JohnDue-auth-value",
     };
     res.send(mockData);
+});
+route.get("/auth", (req, res) => {
+    const token = req.headers.authorization;
+    console.log("token", token);
+    if (!token)
+        res.status(500).json("No token");
+    res.json(token);
 });
 // Use the route
 app.use("/", route);
